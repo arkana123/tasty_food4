13937c3d8c45d020945f4810d3c1110abe4e9a02
arkana123 <arkana3101@gmail.com>
2023-07-06 20:01:54 +0700

    code screens

---
+++ lib/screens/components/login_content.dart
@@ -0,0 +1,315 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:ionicons/ionicons.dart';
+import 'package:tasty_food/screens/home_screen.dart';
+
+import '../../../utils/constants.dart';
+import '../../animations/change_screen_animation.dart';
+import '../../utils/helper_functions.dart';
+import 'bottom_text.dart';
+import 'top_text.dart';
+
+enum Screens {
+  createAccount,
+  welcomeBack,
+}
+
+class LoginContent extends StatefulWidget {
+  const LoginContent({Key? key}) : super(key: key);
+
+  @override
+  State<LoginContent> createState() => _LoginContentState();
+}
+
+class _LoginContentState extends State<LoginContent>
+    with TickerProviderStateMixin {
+  late final List<Widget> createAccountContent;
+  late final List<Widget> loginContent;
+
+  final _formkeySignUp = GlobalKey<FormState>();
+  final _formkeySignIn = GlobalKey<FormState>();
+  final _auth = FirebaseAuth.instance;
+  final TextEditingController passwordController = new TextEditingController();
+  final TextEditingController passwordControllerSignIn =
+      new TextEditingController();
+  final TextEditingController confirmpassController =
+      new TextEditingController();
+  final TextEditingController name = new TextEditingController();
+  final TextEditingController emailController = new TextEditingController();
+  final TextEditingController emailControllerSignIn =
+      new TextEditingController();
+  final TextEditingController mobile = new TextEditingController();
+  bool _isObscure = true;
+  bool _isObscure2 = true;
+
+  Widget inputField(String hint, IconData iconData,
+      TextEditingController textEditingController) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 36, vertical: 8),
+      child: SizedBox(
+        height: 50,
+        child: Material(
+          elevation: 8,
+          shadowColor: Colors.black87,
+          color: Colors.transparent,
+          borderRadius: BorderRadius.circular(30),
+
+          child: TextFormField(
+
+            controller: textEditingController,
+            textAlignVertical: TextAlignVertical.bottom,
+            decoration: InputDecoration(
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(30),
+                borderSide: BorderSide.none,
+              ),
+              filled: true,
+              fillColor: Colors.white,
+              hintText: hint,
+
+              prefixIcon: Icon(iconData),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget inputFieldPassword(String hint, IconData iconData,
+      TextEditingController textEditingController) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 36, vertical: 8),
+      child: SizedBox(
+        height: 50,
+        child: Material(
+          elevation: 8,
+          shadowColor: Colors.black87,
+          color: Colors.transparent,
+          borderRadius: BorderRadius.circular(30),
+
+          child: TextFormField(
+            obscureText: true,
+            enableSuggestions: false,
+            autocorrect: false,
+            controller: textEditingController,
+            textAlignVertical: TextAlignVertical.bottom,
+            decoration: InputDecoration(
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(30),
+                borderSide: BorderSide.none,
+              ),
+              filled: true,
+              fillColor: Colors.white,
+              hintText: hint,
+
+              prefixIcon: Icon(iconData),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget loginButton(String title) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 32, vertical: 16),
+      child: ElevatedButton(
+        onPressed: () {
+          signUp(emailController.text, passwordController.text, "user");
+        },
+        style: ElevatedButton.styleFrom(
+          padding: const EdgeInsets.symmetric(vertical: 14),
+          shape: const StadiumBorder(),
+          primary: kSecondaryColor,
+          elevation: 8,
+          shadowColor: Colors.black87,
+        ),
+        child: Text(
+          title,
+          style: const TextStyle(
+            fontSize: 18,
+            fontWeight: FontWeight.bold,
+            color: Colors.white,
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget orDivider() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 130, vertical: 8),
+      child: Row(
+        children: [
+          Flexible(
+            child: Container(
+              height: 1,
+              color: kPrimaryColor,
+            ),
+          ),
+          const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 16),
+            child: Text(
+              'or',
+              style: TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+          ),
+          Flexible(
+            child: Container(
+              height: 1,
+              color: kPrimaryColor,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget logos() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 16),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Image.asset('assets/images/facebook.png'),
+          const SizedBox(width: 24),
+          Image.asset('assets/images/google.png'),
+        ],
+      ),
+    );
+  }
+
+  Widget forgotPassword() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 110),
+      child: TextButton(
+        onPressed: () {},
+        child: const Text(
+          'Forgot Password?',
+          style: TextStyle(
+            fontSize: 16,
+            fontWeight: FontWeight.w600,
+            color: kSecondaryColor,
+          ),
+        ),
+      ),
+    );
+  }
+
+  @override
+  void initState() {
+    createAccountContent = [
+      inputField('Nama', Ionicons.person_outline, name),
+      inputField('Email', Ionicons.mail_outline, emailController),
+      inputFieldPassword('Password', Ionicons.lock_closed_outline, passwordController),
+      inputFieldPassword('Konfirmasi Password', Ionicons.lock_closed_outline,
+          confirmpassController),
+      loginButton('Sign Up'),
+    ];
+
+    loginContent = [
+      inputField('Email', Ionicons.mail_outline, emailControllerSignIn),
+      inputFieldPassword(
+          'Password', Ionicons.lock_closed_outline, passwordControllerSignIn),
+      loginButton('Log In'),
+      // forgotPassword(),
+    ];
+
+    ChangeScreenAnimation.initialize(
+      vsync: this,
+      createAccountItems: createAccountContent.length,
+      loginItems: loginContent.length,
+    );
+
+    for (var i = 0; i < createAccountContent.length; i++) {
+      createAccountContent[i] = HelperFunctions.wrapWithAnimatedBuilder(
+        animation: ChangeScreenAnimation.createAccountAnimations[i],
+        child: createAccountContent[i],
+      );
+    }
+
+    for (var i = 0; i < loginContent.length; i++) {
+      loginContent[i] = HelperFunctions.wrapWithAnimatedBuilder(
+        animation: ChangeScreenAnimation.loginAnimations[i],
+        child: loginContent[i],
+      );
+    }
+
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    ChangeScreenAnimation.dispose();
+
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        const Positioned(
+          top: 136,
+          left: 24,
+          child: TopText(),
+        ),
+        Padding(
+          padding: const EdgeInsets.only(top: 100),
+          child: Stack(
+            children: [
+              Form(
+                key: _formkeySignUp,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: createAccountContent,
+                ),
+              ),
+              Form(
+                key: _formkeySignIn,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: loginContent,
+                ),
+              ),
+            ],
+          ),
+        ),
+        const Align(
+          alignment: Alignment.bottomCenter,
+          child: Padding(
+            padding: EdgeInsets.only(bottom: 50),
+            child: BottomText(),
+          ),
+        ),
+      ],
+    );
+  }
+
+  void signUp(String email, String password, String rool) async {
+    CircularProgressIndicator();
+    if (_formkeySignUp.currentState!.validate()) {
+      await _auth
+          .createUserWithEmailAndPassword(email: email, password: password)
+          .then((value) => {postDetailsToFirestore(email, rool)})
+          .catchError((e) {});
+    }
+  }
+
+  postDetailsToFirestore(String email, String rool) async {
+    FirebaseFirestore firebaseFirestore = FirebaseFirestore.instance;
+    var user = _auth.currentUser;
+    CollectionReference ref = FirebaseFirestore.instance.collection('users');
+    ref.doc(user!.uid).set({'email': emailController.text, 'rool': rool});
+    Navigator.pushReplacement(
+      context,
+      MaterialPageRoute(builder: (context) => HomeScreen()),
+    );
+  }
+}
