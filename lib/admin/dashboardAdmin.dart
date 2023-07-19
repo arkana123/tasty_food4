6a9e01ce27aa64bc4587da5d6bef95c5831f089d
doradankua20 <138378233+doradankua20@users.noreply.github.com>
2023-07-19 22:57:34 +0700

    Code dashboardAdmin.dart

--- lib/admin/dashboardAdmin.dart
+++ lib/admin/dashboardAdmin.dart
@@ -1,16 +1,215 @@
-
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:tasty_food/admin/kurir/kurir.dart';
+import 'package:tasty_food/admin/laporan/laporan.dart';
+import 'package:tasty_food/admin/restaurant/restaurant.dart';
+import 'package:tasty_food/admin/transaksi/transaksi2.dart';
+import 'package:tasty_food/admin/user/user.dart';
+import 'package:tasty_food/login.dart';
 
-class dashboardAdmin extends StatefulWidget {
-  const dashboardAdmin({super.key});
+import 'menu/menu.dart';
+
+class DashboardAdmin extends StatefulWidget {
+  DashboardAdmin({Key? key}) : super(key: key);
 
   @override
-  State<dashboardAdmin> createState() => _dashboardAdminState();
+  State<DashboardAdmin> createState() => _DashboardAdminState();
 }
 
-class _dashboardAdminState extends State<dashboardAdmin> {
+class _DashboardAdminState extends State<DashboardAdmin> {
+  late User _user; // Objek User yang sedang login
+  late String _displayName = ''; // Nama pengguna
+
+  @override
+  void initState() {
+    super.initState();
+    _user = FirebaseAuth.instance.currentUser!;
+    getUserDisplayName();
+  }
+
+  void getUserDisplayName() async {
+    DocumentSnapshot snapshot = await FirebaseFirestore.instance
+        .collection('users')
+        .doc(_user.uid)
+        .get();
+
+    setState(() {
+      _displayName = snapshot.get('name');
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: ListView(
+        padding: EdgeInsets.zero,
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              color: Theme.of(context).primaryColor,
+              borderRadius: const BorderRadius.only(
+                bottomRight: Radius.circular(50),
+              ),
+            ),
+            child: Column(
+              children: [
+                const SizedBox(height: 50),
+                ListTile(
+                  contentPadding:
+                      const EdgeInsets.symmetric(horizontal: 30.0),
+                  title: Text(
+                    'Hai, $_displayName',
+                    style: Theme.of(context).textTheme.headlineSmall?.copyWith(
+                          color: Colors.white,
+                        ),
+                  ),
+                  subtitle: Text(
+                    'Selamat Datang',
+                    style: Theme.of(context).textTheme.titleMedium?.copyWith(
+                          color: Colors.white54,
+                        ),
+                  ),
+                ),
+                const SizedBox(height: 30),
+              ],
+            ),
+          ),
+          Container(
+            color: Theme.of(context).primaryColor,
+            child: Container(
+              padding: const EdgeInsets.symmetric(horizontal: 30.0),
+              decoration: const BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(100),
+                ),
+              ),
+              child: GridView.count(
+                shrinkWrap: true,
+                crossAxisCount: 2,
+                crossAxisSpacing: 40,
+                mainAxisSpacing: 30,
+                children: [
+                  InkWell(
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => UserManagement(),
+                        ),
+                      );
+                    },
+                    child: itemDashboard("User", CupertinoIcons.person_2, Colors.green),
+                  ),
+                  InkWell(
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => KurirPage(),
+                        ),
+                      );
+                    },
+                    child: itemDashboard("Kurir", CupertinoIcons.group_solid, Colors.blue),
+                  ),
+                  InkWell(
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => MenuPage(),
+                        ),
+                      );
+                    },
+                    child: itemDashboard("Menu", CupertinoIcons.cart, Colors.deepOrange),
+                  ),
+                  InkWell(
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => RestaurantPage(),
+                        ),
+                      );
+                    },
+                    child: itemDashboard("Restaurant", CupertinoIcons.cart, Colors.deepOrange),
+                  ),
+                  InkWell(
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => TransaksiPage(),
+                        ),
+                      );
+                    },
+                    child: itemDashboard("Transaksi", CupertinoIcons.book, Colors.indigo),
+                  ),
+                  InkWell(
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => LaporanTransaksiPage(),
+                        ),
+                      );
+                    },
+                    child: itemDashboard("Laporan", CupertinoIcons.doc, Colors.grey),
+                  ),
+                  InkWell(
+                    onTap: () {
+                      logout(context);
+                    },
+                    child: itemDashboard("Logout", CupertinoIcons.arrow_turn_down_left, Colors.red),
+                  ),
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  itemDashboard(String title, IconData iconData, Color background) => Container(
+        decoration: BoxDecoration(
+            color: Colors.white54,
+            borderRadius: BorderRadius.circular(10),
+            boxShadow: [
+              BoxShadow(
+                  offset: const Offset(0, 5),
+                  color: Theme.of(context).primaryColor.withOpacity(.2),
+                  spreadRadius: 2,
+                  blurRadius: 5)
+            ]),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              padding: const EdgeInsets.all(10),
+              decoration: BoxDecoration(
+                  color: background, shape: BoxShape.circle),
+              child: Icon(iconData, color: Colors.white),
+            ),
+            const SizedBox(height: 8),
+            Text(title, style: Theme.of(context).textTheme.titleMedium),
+          ],
+        ),
+      );
+
+  Future<void> logout(BuildContext context) async {
+    const CircularProgressIndicator();
+    await FirebaseAuth.instance.signOut();
+    // Show a snackbar
+    ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+        content: Text('Anda Telah Keluar!')));
+    Navigator.pushReplacement(
+      context,
+      MaterialPageRoute(
+        builder: (context) => LoginPage(),
+      ),
+    );
   }
 }
